d77f3e87e9573b602b2ae51c554310364f0bea44
AntenehTesfaye <damiteant01@gmail.com>
2022-04-08 19:05:29 -1100

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 char base_16;
-for (base_16 = 40; base_16 <= 57; base_16++)
+for (base_16 = 0; base_16 <= 9; base_16++)
 {
 putchar(base_16);
 }
-for (base_16 = 97; base_16 <= 102; base_16++)
+for (base_16 = 'a'; base_16 <= 'f'; base_16++)
 {
 putchar(base_16);
 }
