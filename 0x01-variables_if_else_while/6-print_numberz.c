0a7b9b545689c6b2ee39c6f3e634858d6fb49e60
AntenehTesfaye <damiteant01@gmail.com>
2022-04-08 17:57:04 -1100

    nubers again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* it excutes and prints it
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 48 ; n <= 57; n++)
+{
+putchar(n);
+}
+putchar("\n");
+return (0);
+}
