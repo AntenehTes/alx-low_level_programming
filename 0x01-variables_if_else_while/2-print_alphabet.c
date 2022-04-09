e26da669a180a7bf464746b95befc0393df107c0
AntenehTesfaye <damiteant01@gmail.com>
2022-04-08 15:44:55 -1100

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,15 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-int c;
 
-for (c = 0; c < 26; ++c)
 {
-putchar("a" + c);
+char alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
 }
 putchar("\n");
+
 return (0);
 }
